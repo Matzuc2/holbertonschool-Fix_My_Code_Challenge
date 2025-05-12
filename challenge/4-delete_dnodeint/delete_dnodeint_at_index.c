32163f205b48f6d498c4c1219fca236bf2ac554a
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-05-13 00:26:12 +0200

    Fix newline at end of file in multiple C and JavaScript files

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -48,6 +48,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		(*head) = (*head)->prev;
         free((*head)->next);
         (*head)->next = ds;
+        (*head)->next->prev = (*head);
 		*head = saved_head;
 	}
 	return (1);
